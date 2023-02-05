42324f1708e0160c61521bcc47cce8325cb2e8aa
XavierZXY <zxy_xavier@outlook.com>
2023-02-05 17:23:36 +0800

    feat: :construction: finish hw2-q2-layernorm1d

--- hw2/python/needle/nn.py
+++ hw2/python/needle/nn.py
@@ -169,12 +169,25 @@ class LayerNorm1d(Module):
         self.dim = dim
         self.eps = eps
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.weight = Parameter(init.ones(self.dim, requires_grad=True))
+        self.bias = Parameter(init.zeros(self.dim, requires_grad=True))
         ### END YOUR SOLUTION
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        batch_size = x.shape[0]
+        feature_size = x.shape[1]
+        # NOTE need reshape, because (4, ) can brcsto (2, 4) but (4, ) can not brcsto (4, 2)
+        mean = x.sum(axes=(1, )).reshape((batch_size, 1)) / feature_size
+
+        # NOTE need manual broadcast_to!!!
+        x_minus_mean = x - mean.broadcast_to(x.shape)
+        x_std = ((x_minus_mean ** 2).sum(axes=(1, )).reshape((batch_size, 1)) / feature_size + self.eps) ** 0.5
+        
+        # NOTE need manual broadcast_to!!!
+        normed  = x_minus_mean / x_std.broadcast_to(x.shape)
+        
+        return self.weight.broadcast_to(x.shape) * normed + self.bias.broadcast_to(x.shape)
         ### END YOUR SOLUTION
 
 
