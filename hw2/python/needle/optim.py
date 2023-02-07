d9bfc4749d0d4e046e8c30aa04cfe8811e1c788d
XavierZXY <zxy_xavier@outlook.com>
2023-02-07 10:57:03 +0800

    feat: :construction: finish HW2-Q3-Adam

--- hw2/python/needle/optim.py
+++ hw2/python/needle/optim.py
@@ -62,5 +62,21 @@ class Adam(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.t += 1
+        for i, param in enumerate(self.params):
+            if i not in self.m:
+                self.m[i] = ndl.init.zeros(*param.shape)
+                self.v[i] = ndl.init.zeros(*param.shape)
+
+            # NOTE: param.grad maybe None
+            if param.grad is None:
+                continue
+            grad_data = ndl.Tensor(param.grad.numpy(), dtype='float32').data + param.data * self.weight_decay
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grad_data
+            self.v[i] = self.beta2 * self.v[i] + (1- self.beta2) * grad_data**2
+
+            # NOTE: bias correction
+            u_hat = (self.m[i]) / (1 - self.beta1 ** self.t)
+            v_hat = (self.v[i]) / (1 - self.beta2 ** self.t)
+            param.data = param.data - self.lr * u_hat / (v_hat ** 0.5 + self.eps)
         ### END YOUR SOLUTION
