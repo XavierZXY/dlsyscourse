9a8aa3d26e4a52a9ac77e4466a4d25d42c68ae1f
XavierZXY <zxy_xavier@outlook.com>
2023-02-08 14:26:18 +0800

    feat: :construction: finish HW2-Q4-transformation

--- hw2/python/needle/data.py
+++ hw2/python/needle/data.py
@@ -1,4 +1,6 @@
 import numpy as np
+import gzip
+import struct
 from .autograd import Tensor
 
 from typing import Iterator, Optional, List, Sized, Union, Iterable, Any
@@ -18,13 +20,18 @@ class RandomFlipHorizontal(Transform):
         Horizonally flip an image, specified as n H x W x C NDArray.
         Args:
             img: H x W x C NDArray of an image
+            H: height
+            W: weight
+            C: channels
         Returns:
             H x W x C ndarray corresponding to image flipped with probability self.p
         Note: use the provided code to provide randomness, for easier testing
         """
         flip_img = np.random.rand() < self.p
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if flip_img:
+            return np.flip(img, axis=1)
+        return img
         ### END YOUR SOLUTION
 
 
@@ -42,7 +49,17 @@ class RandomCrop(Transform):
         """
         shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        result = np.zeros_like(img)
+        H, W = img.shape[0], img.shape[1]
+        # NOTE: when shift out of bounds, just return zeros
+        if abs(shift_x) >= H or abs (shift_y) >= W:
+            return result
+        st_1, ed_1 = max(0, -shift_x), min(H - shift_x, H)
+        st_2, ed_2 = max(0, -shift_y), min(W - shift_y, W)
+        img_st_1, img_ed_1 = max(0, shift_x), min(H + shift_x, H)
+        img_st_2, img_ed_2 = max(0, shift_y), min(W + shift_y, W)
+        result[st_1:ed_1, st_2:ed_2, :] = img[img_st_1:img_ed_1, img_st_2:img_ed_2, :]
+        return result
         ### END YOUR SOLUTION
 
 
@@ -141,3 +158,46 @@ class NDArrayDataset(Dataset):
 
     def __getitem__(self, i) -> object:
         return tuple([a[i] for a in self.arrays])
+
+
+def parse_mnist(image_filename, label_filename):
+    """ Read an images and labels file in MNIST format.  See this page:
+    http://yann.lecun.com/exdb/mnist/ for a description of the file format.
+
+    Args:
+        image_filename (str): name of gzipped images file in MNIST format
+        label_filename (str): name of gzipped labels file in MNIST format
+
+    Returns:
+        Tuple (X,y):
+            X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded 
+                data.  The dimensionality of the data should be 
+                (num_examples x input_dim) where 'input_dim' is the full 
+                dimension of the data, e.g., since MNIST images are 28x28, it 
+                will be 784.  Values should be of type np.float32, and the data 
+                should be normalized to have a minimum value of 0.0 and a 
+                maximum value of 1.0. The normalization should be applied uniformly
+                across the whole dataset, _not_ individual images.
+
+            y (numpy.ndarray[dtype=np.uint8]): 1D numpy array containing the
+                labels of the examples.  Values should be of type np.uint8 and
+                for MNIST will contain the values 0-9.
+    """
+    ### BEGIN YOUR CODE
+
+    ### load image
+    with gzip.open(image_filename) as f:
+      # First 16 bytes are magic_number, n_imgs, n_rows, n_cols
+      pixels = np.frombuffer(f.read(), dtype='uint8', offset=16)
+      # print("type = %s" % pixels[0].dtype)
+      pixels = pixels.reshape(-1, 784).astype('float32') / 255
+      # pixels = pixels.reshape(-1, 784)
+    
+    ### load label
+    with gzip.open(label_filename) as f:
+      # First 8 bytes are magic_number, n_labels
+      integer_labels = np.frombuffer(f.read(), dtype='uint8', offset=8)
+    
+    return (pixels, integer_labels) 
+    ### END YOUR CODE
+
